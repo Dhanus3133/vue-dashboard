809f0e051760cb4541dda561cce9916288fc878f
Dhanus SL <dhanus3133@gmail.com>
2020-09-10 15:55:16 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  background: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold($purple);
+}
+
+#app {
+  font-family: $system-font-family;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $white;
+}
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: $white;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
